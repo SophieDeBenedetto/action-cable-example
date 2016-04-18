304cfd8ae6290c17ac2fc47d31e38e1158f9c997
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2016-04-18 12:14:56 -0400

    remove flash message from page when topic is valid

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,5 +1,5 @@
 class Chatroom < ApplicationRecord
-  has_many :messages
+  has_many :messages, dependent: :destroy
   has_many :users, through: :messages
   validates :topic, presence: true, uniqueness: true, case_sensitive: false
   before_validation :sanitize, :slugify
