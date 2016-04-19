0d7b47c364f34b25a738428cc8fd4fcae4ffeb8c
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2016-04-19 14:08:27 -0400

    remove leftover devise code from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :messages
   has_many :chatrooms, through: :messages
 
