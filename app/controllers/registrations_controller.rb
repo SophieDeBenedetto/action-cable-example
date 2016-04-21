c7f7fe40c77cee9faaa351bcb9ba215ab36a5b40
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2016-04-21 10:33:28 -0400

    skip authentication for registrations and sessions controllers

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,5 @@
 class RegistrationsController < ApplicationController
+  skip_before_action :authenticate_user!
 
   def new
     @user = User.new
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-
+  skip_before_action :authenticate_user!
   skip_before_action :verify_authenticity_token, only: [:destroy]
   
   def new
