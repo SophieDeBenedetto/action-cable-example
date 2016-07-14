8e3a89a3a5c58f6ec10622be1abe89a48eb278ca
Keenan Brock <keenan@thebrocks.net>
2016-07-13 22:06:35 -0400

    auto derive the web socket url for development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,5 +59,4 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.action_cable.url = "ws://localhost:3000/cable" 
 end
